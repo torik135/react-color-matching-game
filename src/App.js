d76c843fca183de451e3c8fce93f1c22f2323332
torik <riqibrekele@gmail.com>
2021-11-06 20:34:08 +0700

    add check row of 3 and 4

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
       // i = 0
       // i + width(8) = 8
       // i + width(8) * 2 = 16
-      // so it make 3 item verticaly
+      // so it make 3 items verticaly
       // and the value of i will keep changing based on the index / box that we choose
       const column3 = [i, i + width, i + width * 2]
 
@@ -47,7 +47,7 @@ const App = () => {
       // i + width(8) = 8
       // i + width(8) * 2 = 16
       // i + width(8) * 3 = 24
-      // so it make 4 item verticaly
+      // so it make 4 items verticaly
       // and the value of i will keep changing based on the index / box that we choose
       const column4 = [i, i + width, i + width * 2, i + width * 3]
 
@@ -63,6 +63,121 @@ const App = () => {
     }
   }
 
+  // checking of row 4 and 3
+  const checkRow3 = () => {
+    // 64 = index of the row of 3 that get check
+    // after index number 64 the row is 2 not 3 anymore
+    for (let i = 0; i < 64; i++) {
+      // i = 0
+      // i + 1
+      // i + 2
+      // so it make 3 items horizontaly
+      // and the value of i will keep changing based on the index / box that we choose
+      const row4 = [i, i + 1, i + 2]
+
+      // getting the first value of the column
+      const decidedColor = currColorArr[i]
+
+      // no valid box / index
+      // this box / index will not be check because is 2 not 3 anymore
+      const notValidBox = [
+        6, 7,
+        14, 15,
+        22, 23,
+        30, 31,
+        38, 39,
+        46, 47,
+        54, 55,
+        63, 64
+      ]
+
+      // if value of i is matching the value of notValidBox then
+      // continue the code
+      if (notValidBox.includes(i)) continue
+
+      // match checking
+      // if every box if match with decidedColor(first color) then
+      // replace it with blank box
+      if (row4.every(box => currColorArr[box] === decidedColor)) {
+        row4.forEach(box => { currColorArr[box] = '' })
+      }
+    }
+  }
+
+  const checkRow4 = () => {
+    // 39 = index of the column of 3 that get check
+    // after index number 64 the column is 2 not 3 anymore
+    for (let i = 0; i < 64; i++) {
+      // i = 0
+      // i + 1
+      // i + 2
+      // i + 3
+      // so it make 4 item horizontaly
+      // and the value of i will keep changing based on the index / box that we choose
+      const row4 = [i, i + 1, i + 2, i + 3]
+
+      // getting the first value of the column
+      const decidedColor = currColorArr[i]
+
+      // no valid box / index
+      // this box / index will not be check because is 3 not 4 anymore
+      const notValidBox = [
+        5, 6, 7,
+        13, 14, 15,
+        21, 22, 23,
+        29, 30, 31,
+        3738, 39,
+        45, 46, 47,
+        53, 54, 55,
+        62, 63, 64
+      ]
+
+      // if value of i is matching the value of notValidBox then
+      // continue the code
+      if (notValidBox.includes(i)) continue
+
+      // match checking
+      // if every box if match with decidedColor(first color) then
+      // replace it with blank box
+      if (row4.every(box => currColorArr[box] === decidedColor)) {
+        row4.forEach(box => { currColorArr[box] = '' })
+      }
+    }
+  }
+
+  // move and replace the box
+  const moveBoxBelow = () => {
+    // looping all the box
+    for (let i = 0; i < 64; i++) {
+      // top box
+      const topBox = [0, 1, 2, 3, 4, 5, 6, 7]
+      // 
+      const isTopBox = topBox.includes(i)
+
+      // if topbox is blank
+      if (isTopBox && currColorArr[i] === '') {
+        // fillin blank box with random color
+        const randomNum = Math.floor(Math.random() * candyColors.length)
+        currColorArr[i] = candyColors[randomNum]
+
+      }
+
+      // if the box below is empty
+      // then move it down to the blank box
+      if ((currColorArr[i + width]) === '') {
+        // replace blank box with colored box above it
+        // move blank up
+        currColorArr[i + width] = currColorArr[i]
+        // replace the colored box with blank box below it
+        // move color down
+        currColorArr[i] = ''
+      }
+    }
+  }
+
+  const dragStart = () => { console.log('dragStart') }
+  const dragDrop = () => { console.log('dragDrop') }
+  const dragEnd = () => { console.log('dragEnf') }
 
   // create an array 64 item (8*8)
   const createBoard = () => {
@@ -98,12 +213,19 @@ const App = () => {
     const timer = setInterval(() => {
       checkColumn4()
       checkColumn3()
+      checkRow4()
+      checkRow3()
+      moveBoxBelow()
       // set the current color arrangement with the new color arrangement
       setCurrColorArr([...currColorArr])
     }, 100)
 
     return () => clearInterval(timer)
-  }, [checkColumn4, checkColumn3, currColorArr])
+  }, [
+    checkColumn4, checkRow4,
+    checkColumn3, checkRow3,
+    moveBoxBelow,
+    currColorArr])
 
   return (
     <div className="app">
@@ -115,8 +237,16 @@ const App = () => {
 
           <img
             key={index}
+            data-id={index}
             style={{ backgroundColor: candyColor }}
-            alt={candyColor}
+            alt={index}
+            draggable={true}
+            onDragStart={dragStart}
+            onDragOver={(e) => e.preventDefault()}
+            onDragEnter={(e) => e.preventDefault()}
+            onDragLeave={(e) => e.preventDefault()}
+            onDrop={dragDrop}
+            onDragEnd={dragEnd}
           />
 
         ))}
